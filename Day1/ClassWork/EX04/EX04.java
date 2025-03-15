a349e4683242a79378d2ee3bbbfd7c3a7ad06a67
lakkula <33682104+lakkula@users.noreply.github.com>
2025-03-15 09:19:01 +0530

    Upload Day1 class work EX04

---
+++ Day1/ClassWork/EX04/EX04.java
@@ -0,0 +1,15 @@
+class EX04{
+	public static void main(String[] args){
+		System.out.println("Name: Kulani Lakshika");
+		System.out.println("Registration Number: 2022/ASP/25");
+		System.out.println("Degree Programm: BSc.Applied Mathematics and Compiting");
+		
+		String Name = "Kulani Lakshika";
+		String RegNo = "2022/ASP/25";
+		String Degree = "BSc.Applied Mathematics and Compiting";
+		
+		System.out.println("Full Name: " + Name);
+		System.out.println("Registration Number: " + RegNo);
+		System.out.println("Degree Programm: " + Degree);
+	}
+}

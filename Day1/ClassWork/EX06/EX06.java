f1be1f9cff209cbbd5b0aaabc7836eee16fdd184
lakkula <33682104+lakkula@users.noreply.github.com>
2025-03-15 09:31:59 +0530

    Upload Day1 class work EX06

---
+++ Day1/ClassWork/EX06/EX06.java
@@ -0,0 +1,12 @@
+class EX06{
+	public static void main(String[] args){
+		int num1 = 55;
+		int num2 = 20;
+		
+		System.out.println("Addition is " + (num1+num2));
+		System.out.println("Substraction is " + (num1-num2));
+		System.out.println("Multiplication is " + (num1*num2));
+		System.out.println("Division is " + ((double)num1/(double)num2));
+		System.out.println("Mode is " + (num1%num2));
+	}
+}
